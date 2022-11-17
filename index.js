0968dfaf55706a05faa79e5498e6b8a3e18b6526
Marlon Baker <marbaker.swe@gmail.com>
2022-11-16 19:38:34 -0800

    version 1

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom){
+
+let image = document.createElement('img')
+image.src = url
+image.style.position = 'fixed'
+image.style.left = left + 'px'
+image.style.bottom = bottom + 'px'
+document.body.append(image)
+return image
+    
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+
+
